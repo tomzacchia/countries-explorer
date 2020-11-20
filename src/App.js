604bbd0187d90bef1f50d95d409a34a690949e8d
tomzacchia <tomzacchia@live.com>
2020-11-20 13:27:11 -0500

    Add reach router for routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { Router } from "@reach/router";
 import Header from "./components/header/header";
 import GlobalStyles from "./global.styles";
 import { lightTheme, darkTheme } from "./styles/theme";
 import { useDarkMode } from "./useDarkMode";
+import HomePage from "./pages/home-page/home-page";
+import Details from "./pages/home-page/details/details";
 
 function App() {
   var [theme, toggleTheme] = useDarkMode();
@@ -13,6 +16,10 @@ function App() {
     <ThemeProvider theme={themeMode}>
       <GlobalStyles />
       <Header toggleTheme={toggleTheme} theme={theme} />
+      <Router>
+        <HomePage path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </ThemeProvider>
   );
 }
