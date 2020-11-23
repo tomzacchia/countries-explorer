114c2c54220ad142474a25aa83b6647647b24cfd
tomzacchia <tomzacchia@live.com>
2020-11-22 21:29:32 -0500

    Add PreviousRouteButton to Details

--- src/pages/details/details.js
+++ src/pages/details/details.js
@@ -1,7 +1,17 @@
 import React from "react";
+import PreviousRouteButton from "../../components/previous-route-button/previous-route-button";
+import * as Styles from "./details.styles";
 
 const Details = () => {
-  return <div>DETAILS</div>;
+  return (
+    <div>
+      <Styles.IconContainer>
+        <PreviousRouteButton message="Back" />
+      </Styles.IconContainer>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default Details;
--- src/pages/details/details.styles.js
+++ src/pages/details/details.styles.js
@@ -1,1 +1,8 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+
+export const IconContainer = styled.div`
+  width: max-content;
+  position: fixed;
+  top: 75px;
+  left: 25px;
+`;
