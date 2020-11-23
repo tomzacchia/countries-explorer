0702fa8ad7196d78789a5418ef8f1795e490f54f
tomzacchia <tomzacchia@live.com>
2020-11-22 22:22:42 -0500

    Add responsivess to homePage form

--- src/pages/details/details.js
+++ src/pages/details/details.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../api";
 import PreviousRouteButton from "../../components/previous-route-button/previous-route-button";
 import * as Styles from "./details.styles";
 
-const Details = () => {
+const Details = ({ alphaCode }) => {
+  var [country, setCountry] = useState("");
+
+  useEffect(() => {
+    API.get(`alpha/${alphaCode}`).then(
+      ({ data }) => setCountry(data),
+      console.error
+    );
+  }, []);
+
   return (
     <div>
       <Styles.IconContainer>
         <PreviousRouteButton message="Back" />
       </Styles.IconContainer>
 
-      <div></div>
+      <div>{!country ? <h1>Loading</h1> : <h1>{country.name}</h1>}</div>
     </div>
   );
 };
