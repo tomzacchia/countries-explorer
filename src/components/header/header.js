04d60b2fc6bd45511e03fe1bd69c367ca2671d93
tomzacchia <tomzacchia@live.com>
2020-12-03 14:38:10 -0500

    Add onClick to header title for routeing to home page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import * as Styles from "./header.styles";
 import { ButtonNoShadow } from "../../styles/buttons";
+import { navigate } from "@reach/router";
 
 const Header = ({ toggleTheme, theme }) => {
   const iconClassname = theme === "light" ? "far fa-moon" : "fas fa-moon";
 
   return (
     <Styles.HeaderContainer>
-      <Styles.HeaderTitle> Where in the world? </Styles.HeaderTitle>
+      <Styles.HeaderTitle onClick={() => navigate("/")}>
+        Where in the world?
+      </Styles.HeaderTitle>
 
       <ButtonNoShadow className="withIcon" onClick={toggleTheme}>
         <i className={iconClassname}></i>
